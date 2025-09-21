dd2a449ed6af4e851fc685138149d814eadaa5a8
HC-SILVA <cristinahellen216@gmail.com>
2025-09-21 16:19:33 -0300

    Commit Correcao de arquivos

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 alert('Olá aventureiro!');
+
+
+function mostrarMenu() {
+    if ( itens.style.display == 'block' ) {
+        itens.style.display = 'none'
+    }
+    else {
+        itens.style.display = "block"
+    }
+    
+}
